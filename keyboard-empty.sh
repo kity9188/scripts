c0f44e29f6a63f96a47c25fd7df5253967a455d8
nicks9188 <nicks9188@gmail.com>
2019-07-07 06:53:38 +0000

    Till-3

--- keyboard-empty.sh
+++ keyboard-empty.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/bash
+# Without assgining any variable
 echo "Enter something: "
 read
 echo You have written: $REPLY
+# Now by assigning a variable "text"
 read -sp "Now try writing something else:" text
 echo
 echo You have written: $text
